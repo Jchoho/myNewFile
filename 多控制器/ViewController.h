f16b676d35b57879d41aa55549c2e6b8b8bebdd1
tan <tan@macbookprodemacbook-pro.local>
2016-01-27 10:09:47 +0800

    Initial Commit

---
+++ 多控制器/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  多控制器
+//
+//  Created by tan on 16/1/27.
+//  Copyright © 2016年 jc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 多控制器/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  多控制器
+//
+//  Created by tan on 16/1/27.
+//  Copyright © 2016年 jc. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
